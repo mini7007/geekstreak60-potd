e359e961937d899b49acc58c847e364c6d88fee4
Mohan Sharma <61463072+mini7007@users.noreply.github.com>
2026-03-15 11:52:18 +0530

    Create Day25_Top_View_of_Binary_Tree.cpp
    Day 25: Top View of Binary Tree | BFS + Horizontal Distance

---
+++ Day25_Top_View_of_Binary_Tree.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> topView(Node *root) {
+        vector<int> ans;
+        if(!root) return ans;
+
+        map<int,int> mp;
+        queue<pair<Node*,int>> q;
+
+        q.push({root,0});
+
+        while(!q.empty()){
+            auto front = q.front();
+            q.pop();
+
+            Node* node = front.first;
+            int hd = front.second;
+
+            if(mp.find(hd) == mp.end())
+                mp[hd] = node->data;
+
+            if(node->left)
+                q.push({node->left, hd-1});
+
+            if(node->right)
+                q.push({node->right, hd+1});
+        }
+
+        for(auto it : mp)
+            ans.push_back(it.second);
+
+        return ans;
+    }
+};
