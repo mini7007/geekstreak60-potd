0abc380685567f6f513add2ba6a26b8a57e1d84e
Mohan Sharma <61463072+mini7007@users.noreply.github.com>
2026-03-18 16:22:52 +0530

    Create Day29_Distribute_Candies.cpp
    Day 29: Distribute Candies | Postorder DFS (Tree Balancing)

---
+++ Day29_Distribute_Candies.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    
+    int moves = 0;
+
+    int dfs(Node* root) {
+        if(!root) return 0;
+
+        int left = dfs(root->left);
+        int right = dfs(root->right);
+
+        moves += abs(left) + abs(right);
+
+        return root->data + left + right - 1;
+    }
+
+    int distCandy(Node* root) {
+        dfs(root);
+        return moves;
+    }
+};
