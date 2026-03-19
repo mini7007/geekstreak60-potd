83381d3de76e30a6b6b02daaf25f2ac62b8f9851
Mohan Sharma <61463072+mini7007@users.noreply.github.com>
2026-03-19 11:08:17 +0530

    Create Day30_Largest_BST_in_Binary_Tree.cpp
    Day 30: Largest BST in Binary Tree | Postorder DFS + Tree DP

---
+++ Day30_Largest_BST_in_Binary_Tree.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+
+    struct Info {
+        bool isBST;
+        int size;
+        int minVal;
+        int maxVal;
+    };
+
+    Info solve(Node* root, int &ans) {
+        if(!root) {
+            return {true, 0, INT_MAX, INT_MIN};
+        }
+
+        Info left = solve(root->left, ans);
+        Info right = solve(root->right, ans);
+
+        Info curr;
+
+        if(left.isBST && right.isBST && 
+           root->data > left.maxVal && root->data < right.minVal) {
+            
+            curr.isBST = true;
+            curr.size = left.size + right.size + 1;
+            curr.minVal = min(root->data, left.minVal);
+            curr.maxVal = max(root->data, right.maxVal);
+
+            ans = max(ans, curr.size);
+        } else {
+            curr.isBST = false;
+            curr.size = max(left.size, right.size);
+            curr.minVal = INT_MIN;
+            curr.maxVal = INT_MAX;
+        }
+
+        return curr;
+    }
+
+    int largestBst(Node *root) {
+        int ans = 0;
+        solve(root, ans);
+        return ans;
+    }
+};
