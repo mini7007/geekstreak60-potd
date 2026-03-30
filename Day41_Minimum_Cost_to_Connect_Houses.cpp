00cc2372d42115e813475dfb39a9fd42ec653b7b
Mohan Sharma <61463072+mini7007@users.noreply.github.com>
2026-03-30 19:39:04 +0530

    Add solution for minimum cost to connect houses
    Implement Dijkstra's algorithm to calculate the minimum cost to connect houses based on their coordinates.
    Day 41: Minimum Cost to Connect Houses | Prim's Algorithm (MST)

---
+++ Day41_Minimum_Cost_to_Connect_Houses.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int minCost(vector<vector<int>>& houses) {
+        
+        int n = houses.size();
+
+        vector<int> minDist(n, INT_MAX);
+        vector<bool> vis(n, false);
+
+        minDist[0] = 0;
+        int cost = 0;
+
+        for(int i = 0; i < n; i++) {
+            
+            int u = -1;
+
+            // pick minimum distance node
+            for(int j = 0; j < n; j++) {
+                if(!vis[j] && (u == -1 || minDist[j] < minDist[u])) {
+                    u = j;
+                }
+            }
+
+            vis[u] = true;
+            cost += minDist[u];
+
+            // update distances
+            for(int v = 0; v < n; v++) {
+                if(!vis[v]) {
+                    int dist = abs(houses[u][0] - houses[v][0]) +
+                               abs(houses[u][1] - houses[v][1]);
+
+                    minDist[v] = min(minDist[v], dist);
+                }
+            }
+        }
+
+        return cost;
+    }
+};
