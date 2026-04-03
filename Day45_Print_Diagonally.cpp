5a263fd43f111d0e44751c0f7f57fc2f41da8826
Mohan Sharma <61463072+mini7007@users.noreply.github.com>
2026-04-03 11:58:06 +0530

    Implement diagonal view of a matrix
    Day 45: Print Diagonally | Matrix Anti-Diagonal Traversal

---
+++ Day45_Print_Diagonally.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> diagView(vector<vector<int>> mat) {
+        
+        int n = mat.size();
+        vector<int> ans;
+
+        for(int sum = 0; sum <= 2*(n-1); sum++) {
+            for(int i = 0; i < n; i++) {
+                int j = sum - i;
+
+                if(j >= 0 && j < n) {
+                    ans.push_back(mat[i][j]);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
