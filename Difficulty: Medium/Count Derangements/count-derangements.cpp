7be8e388f5a83dbb0bce13841122ff7a4db46105
Mohan Sharma <61463072+mini7007@users.noreply.github.com>
2026-04-20 18:39:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Derangements/count-derangements.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int derangeCount(int n) {
+        
+        if(n == 1) return 0;
+        if(n == 2) return 1;
+
+        int prev2 = 0; // D(1)
+        int prev1 = 1; // D(2)
+
+        for(int i = 3; i <= n; i++) {
+            int curr = (i - 1) * (prev1 + prev2);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+};
