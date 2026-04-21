8589111a475ab5b8f26680a825c8ce367f9a7063
Mohan Sharma <61463072+mini7007@users.noreply.github.com>
2026-04-21 16:35:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Two water Jug problem/two-water-jug-problem.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+
+    int pour(int fromCap, int toCap, int d) {
+        int from = fromCap;
+        int to = 0;
+        int step = 1;
+
+        while(from != d && to != d) {
+            int temp = min(from, toCap - to);
+            to += temp;
+            from -= temp;
+            step++;
+
+            if(from == d || to == d) break;
+
+            if(from == 0) {
+                from = fromCap;
+                step++;
+            }
+
+            if(to == toCap) {
+                to = 0;
+                step++;
+            }
+        }
+
+        return step;
+    }
+
+    int minSteps(int m, int n, int d) {
+        
+        if(d > max(m, n)) return -1;
+        if(d % __gcd(m, n) != 0) return -1;
+
+        return min(pour(m, n, d), pour(n, m, d));
+    }
+};
